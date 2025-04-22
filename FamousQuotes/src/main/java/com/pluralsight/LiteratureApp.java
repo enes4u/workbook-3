4399a37d7eefac40fd114d0bac01f0501649e45d
enes4u <enesuzunyearup@gmail.com>
2025-04-22 08:31:54 -0400

    yayy! i believe it works

--- FamousQuotes/src/main/java/com/pluralsight/LiteratureApp.java
+++ FamousQuotes/src/main/java/com/pluralsight/LiteratureApp.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class LiteratureApp {
@@ -17,21 +18,31 @@ public class LiteratureApp {
                 "If you want to live a happy life, tie it to a goal, not to people or things.",
                 "The purpose of life is to believe, to hope, and to strive."
         };
-
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         while (true) {
             try {
-                System.out.print("Enter a number between 1 and 10 to see a quote (or 0 to quit): ");
+                System.out.println("\nOptions:");
+                System.out.println("1 - Select a quote by number (1-10)");
+                System.out.println("2 - Get a random quote");
+                System.out.println("0 - Quit");
+                System.out.print("Enter your choice: ");
                 int choice = scanner.nextInt();
 
                 if (choice == 0) {
                     System.out.println("Exiting the program. Goodbye!");
                     break;
+                } else if (choice == 1) {
+                    System.out.print("Enter a number between 1 and 10: ");
+                    int quoteNumber = scanner.nextInt();
+                    System.out.println("Quote: " + quotes[quoteNumber - 1]);
+                } else if (choice == 2) {
+                    int randomIndex = random.nextInt(quotes.length);
+                    System.out.println("Random Quote: " + quotes[randomIndex]);
+                } else {
+                    System.out.println("Invalid option! Please try again.");
                 }
-
-                // Display the selected quote
-                System.out.println("Quote: " + quotes[choice - 1]);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Invalid choice! Please select a number between 1 and 10.");
             } catch (Exception e) {
